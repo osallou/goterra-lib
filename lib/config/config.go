115dbc3f4b535c63c8e281e1be0ab84f5276bfd1
Olivier Sallou <olivier.sallou@irisa.fr>
2019-06-05 18:31:36 +0200

    add consul registration with GOT_CONSUL=host:8500

--- lib/config/config.go
+++ lib/config/config.go
@@ -1,10 +1,12 @@
 package terraconfig
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 
+	consul "github.com/hashicorp/consul/api"
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -76,4 +78,46 @@ func LoadConfig() Config {
 	}
 	cfg = config
 	return config
+}
+
+// ConsulDeclare declare current service to consul
+func ConsulDeclare(serviceName string, path string) error {
+	// cfg := LoadConfig()
+	if os.Getenv("GOT_CONSUL") != "" {
+		fmt.Printf("Declare service to consul at %s\n", os.Getenv("GOT_CONSUL"))
+		consulCfg := consul.DefaultConfig()
+		consulCfg.Address = os.Getenv("GOT_CONSUL")
+		client, err := consul.NewClient(consulCfg)
+		if err != nil {
+			return err
+		}
+		hostname, _ := os.Hostname()
+		tags := []string{
+			"got",
+			"api",
+			"traefik.backend=" + serviceName,
+			"traefik.frontend.rule=PathPrefix:" + path,
+			"traefik.enable=true",
+		}
+		check := &consul.AgentServiceCheck{
+			CheckID:  hostname,
+			HTTP:     fmt.Sprintf("http://%s:%d%s", hostname, cfg.Web.Port, path),
+			Interval: "30s",
+		}
+		service := &consul.AgentServiceRegistration{
+			ID:      hostname,
+			Address: hostname,
+			Name:    serviceName,
+			Port:    int(cfg.Web.Port),
+			Tags:    tags,
+			Check:   check,
+		}
+		regerr := client.Agent().ServiceRegister(service)
+		if regerr != nil {
+			return regerr
+		}
+		fmt.Printf("service register in consul to handle api calls to %s\n", path)
+	}
+	return nil
+
 }
