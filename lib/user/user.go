d6a840b0ce0a76c74f150bdf21f3f3995c483b12
Olivier Sallou <olivier.sallou@irisa.fr>
2019-06-11 10:01:27 +0200

    update user api call

--- lib/user/user.go
+++ lib/user/user.go
@@ -1,7 +1,6 @@
 package goterrauser
 
 import (
-	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -21,15 +20,15 @@ type User struct {
 	// Namespaces map[string]bool `json:"namespaces"` // map of namespace names, if true user is owner of namespace else only a member
 }
 
-// APIData is message for auth service url /api/auth
-type APIData struct {
-	Key string `json:"key"`
+// AuthData is result struct for authentication with user data and an authentication token
+type AuthData struct {
+	User  User
+	Token []byte
 }
 
 // Check checks X-API-Key authorization content and returns user info
-func Check(apiKey string) (user User, err error) {
-	err = nil
-	user = User{}
+func Check(apiKey string) (AuthData, error) {
+	autData := AuthData{}
 
 	url := os.Getenv("GOT_PROXY")
 	if os.Getenv("GOT_PROXY_AUTH") != "" {
@@ -38,20 +37,19 @@ func Check(apiKey string) (user User, err error) {
 
 	client := &http.Client{}
 	remote := []string{url, "auth", "api"}
-	data := APIData{Key: apiKey}
-	jsonData := new(bytes.Buffer)
-	json.NewEncoder(jsonData).Encode(data)
-	req, _ := http.NewRequest("POST", strings.Join(remote, "/"), jsonData)
+
+	req, _ := http.NewRequest("GET", strings.Join(remote, "/"), nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", apiKey))
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := client.Do(req)
 	if err != nil {
-		return user, errors.New("failed to contact auth service")
+		return autData, errors.New("failed to contact auth service")
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
-		return user, fmt.Errorf("auth error %d", resp.StatusCode)
+		return autData, fmt.Errorf("auth error %d", resp.StatusCode)
 	}
-	respData := &User{}
+	respData := &AuthData{}
 	json.NewDecoder(resp.Body).Decode(respData)
 	return *respData, err
 }
