37c41cfabe2f32f3744ee7ed114d0c87df56e33e
Olivier Sallou <olivier.sallou@irisa.fr>
2019-08-06 15:21:36 +0200

    add useraction to model

--- lib/model/model.go
+++ lib/model/model.go
@@ -157,6 +157,13 @@ type RunAction struct {
 	Secrets map[string]string `json:"secrets"`
 }
 
+// UserAction is message sent to track events to gotevent exchange..
+type UserAction struct {
+	Action string
+	UID    string
+	Data   string
+}
+
 // NSData represent a namespace
 type NSData struct {
 	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
